b05e042c4ffe176adbb9444bd672944971804fb6
Jonathan Funk <jonfunk88@hotmail.com>
2020-02-15 10:35:51 -0800

    styled wordcloud

--- src/App.js
+++ src/App.js
@@ -9,7 +9,21 @@ class App extends Component {
   };
 
   render() {
-    console.log(this.state.words);
+    const options = {
+      colors: ['#2D3748', '#4A5568', '#718096', '#A0AEC0'],
+      enableTooltip: true,
+      deterministic: false,
+      fontFamily: 'oswald',
+      fontSizes: [24, 96],
+      fontStyle: 'normal',
+      fontWeight: 'normal',
+      padding: 1,
+      rotations: 2,
+      rotationAngles: [0, 90],
+      scale: 'linear',
+      spiral: 'rectangular',
+      transitionDuration: 1000
+    };
 
     return (
       <div className="app">
@@ -22,7 +36,7 @@ class App extends Component {
         </header>
         <section className="app-section">
           <div className="container">
-            <ReactWordcloud words={this.state.words} />
+            <ReactWordcloud options={options} words={this.state.words} />
           </div>
         </section>
       </div>
