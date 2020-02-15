a880247e24e9a20bdde1038ab6fdc3c2b41ce9e6
Jonathan Funk <jonfunk88@hotmail.com>
2020-02-15 10:13:40 -0800

    installed react-wordcloud

--- src/App.js
+++ src/App.js
@@ -1,19 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import words from './words';
+import ReactWordcloud from 'react-wordcloud';
 
-function App() {
-  return (
-    <div className="app">
-      <header className="app-header">
-        <h1>Subreddit Wordcloud Generator</h1>
-        <p className="subtitle">
-          Upload your favourite subreddit and get a visual of the most commonly
-          used words
-        </p>
-      </header>
-      <section className="app-section"></section>
-    </div>
-  );
+class App extends Component {
+  state = {
+    words: words
+  };
+
+  render() {
+    console.log(this.state.words);
+
+    return (
+      <div className="app">
+        <header className="app-header">
+          <h1>Subreddit Wordcloud Generator</h1>
+          <p className="subtitle">
+            Upload your favourite subreddit and get a visual of the most
+            commonly used words
+          </p>
+        </header>
+        <section className="app-section">
+          <div className="container">
+            <ReactWordcloud words={this.state.words} />
+          </div>
+        </section>
+      </div>
+    );
+  }
 }
 
 export default App;
