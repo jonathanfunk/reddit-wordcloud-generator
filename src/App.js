298f08b52ccc640cfaebd0c54f8179f0c223572f
Jonathan Funk <jonfunk88@hotmail.com>
2020-02-15 09:56:38 -0800

    started on the styles & header

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div className="app">
+      <header className="app-header">
+        <h1>Subreddit Wordcloud Generator</h1>
+        <p className="subtitle">
+          Upload your favourite subreddit and get a visual of the most commonly
+          used words
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <section className="app-section"></section>
     </div>
   );
 }
