a5cd7bebdbfca9a9173418306aa400b957f7e8ca
Jonathan Funk <jonfunk88@hotmail.com>
2020-02-15 15:41:53 -0800

    cloud no longer rerendners on state change

--- src/App.js
+++ src/App.js
@@ -1,30 +1,47 @@
 import React, { Component } from 'react';
 import './App.css';
-import words from './words';
 import ReactWordcloud from 'react-wordcloud';
+import words from './words';
+
+const options = {
+  colors: ['#1A202C', '#2D3748', '#4A5568', '#718096', '#A0AEC0'],
+  enableTooltip: true,
+  deterministic: false,
+  fontFamily: 'oswald',
+  fontSizes: [24, 96],
+  fontStyle: 'normal',
+  fontWeight: 'normal',
+  padding: 1,
+  rotations: 2,
+  rotationAngles: [0, 90],
+  scale: 'linear',
+  spiral: 'rectangular',
+  transitionDuration: 1000
+};
 
 class App extends Component {
   state = {
-    words: words
+    words: words,
+    subreddit: '',
+    working: false
+  };
+
+  onChange = e => {
+    e.preventDefault();
+    this.setState({
+      subreddit: e.target.value
+    });
+  };
+
+  onSubmit = e => {
+    e.preventDefault();
+    console.log(this.state.subreddit);
+    this.setState({
+      working: false
+    });
   };
 
   render() {
-    const options = {
-      colors: ['#1A202C', '#2D3748', '#4A5568', '#718096', '#A0AEC0'],
-      enableTooltip: true,
-      deterministic: false,
-      fontFamily: 'oswald',
-      fontSizes: [24, 96],
-      fontStyle: 'normal',
-      fontWeight: 'normal',
-      padding: 1,
-      rotations: 2,
-      rotationAngles: [0, 90],
-      scale: 'linear',
-      spiral: 'rectangular',
-      transitionDuration: 1000
-    };
-
     return (
       <div className="app">
         <header className="app-header">
@@ -33,6 +50,15 @@ class App extends Component {
             Upload your favourite subreddit and get a visual of the most
             commonly used words
           </p>
+          <form className="subreddit-input" onSubmit={this.onSubmit}>
+            <input
+              type="text"
+              name="subreddit"
+              value={this.state.subreddit}
+              onChange={this.onChange}
+            />
+            <button type="submit">Generate Wordcloud</button>
+          </form>
         </header>
         <section className="app-section">
           <div className="container">
