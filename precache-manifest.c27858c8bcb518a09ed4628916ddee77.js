2a56aa66534b18a54d365de910a10094c4db46f8
Jonathan Funk <jonfunk88@hotmail.com>
2020-02-17 12:22:27 -0800

    Updates

---
+++ precache-manifest.c27858c8bcb518a09ed4628916ddee77.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fb2026cdc8d4423c70ee6b202eb5f30",
+    "url": "/reddit-wordcloud-generator/index.html"
+  },
+  {
+    "revision": "dc683fb0e3f776bd30a3",
+    "url": "/reddit-wordcloud-generator/static/css/main.b5a6332c.chunk.css"
+  },
+  {
+    "revision": "1cdf0bd806d4118124f0",
+    "url": "/reddit-wordcloud-generator/static/js/2.508ee5e6.chunk.js"
+  },
+  {
+    "revision": "334b4b987003d90b9c311ba0ca8dd3aa",
+    "url": "/reddit-wordcloud-generator/static/js/2.508ee5e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc683fb0e3f776bd30a3",
+    "url": "/reddit-wordcloud-generator/static/js/main.3af20b58.chunk.js"
+  },
+  {
+    "revision": "4fd45bcca425d5dd8e8f",
+    "url": "/reddit-wordcloud-generator/static/js/runtime-main.a2a1ff1d.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/reddit-wordcloud-generator/precache-manifest.c27858c8bcb518a09ed4628916ddee77.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/reddit-wordcloud-generator/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
